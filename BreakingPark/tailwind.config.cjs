0c13c8ca0bd1028ecf760555026bb9f5836d46cf
Elvendetacos <reversdead@gmail.com>
2023-03-25 15:39:11 -0600

    update-commit

--- BreakingPark/tailwind.config.cjs
+++ BreakingPark/tailwind.config.cjs
@@ -5,7 +5,29 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily:{
+      'bolder': ['Bayon', 'sans-serif']
+    },
+    extend: {
+      gridTemplateColumns: {
+        '12r': 'repeat(12, minmax(0, 32px))',
+      },
+      keyframes:{
+        wiggle : {
+          '0%': { height: '1rem'},
+          '10%': {  height: '5rem' },
+          '20%': { height: '10rem' },
+        },
+        text: {
+          '0%': {'font-size': '1px'}, 
+          '10%': {'font-size': '2px'} 
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 0.3s ease-out',
+        text: 'text 0.1s ease-out'
+      }
+    }
   },
   plugins: [],
 }
